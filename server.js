463826d818d01d786f37d214c936a546fb6156b9
j4v3d <javedrealtor@gmail.com>
2023-02-07 23:00:12 -0500

    set up Routes

--- server.js
+++ server.js
@@ -2,38 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 //Load config file (env vars)
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
-
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, message: 'Show all bootcamps' });
-});
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Show bootcamp ${req.params.id}` });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(201).json({ success: true, message: 'Create a new boot camps' });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Delete bootcamp ${req.params.id}` });
-});
 
 app.listen(
   PORT,
