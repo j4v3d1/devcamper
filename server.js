411f859e5cf153719db35f17130c6d344dc0177a
Muhammad Javed <javedrealtor@gmail.com>
2023-02-07 22:46:58 -0500

    basic v1 bootcamp RESTFUL API

--- server.js
+++ server.js
@@ -9,6 +9,32 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, message: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Show bootcamp ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(201).json({ success: true, message: 'Create a new boot camps' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Delete bootcamp ${req.params.id}` });
+});
+
 app.listen(
   PORT,
   console.log(
