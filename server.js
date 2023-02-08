f69f746e75e5c9f8ec4a43435fbfc4546bcf0bbe
j4v3d <javedrealtor@gmail.com>
2023-02-07 23:51:09 -0500

    install morgan

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
-const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
@@ -11,7 +11,11 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.use(logger);
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
