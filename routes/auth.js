5df7b81443ee94c22330b73dd0608e3936f77023
j4v3d <javedrealtor@gmail.com>
2023-02-10 20:45:22 -0500

    User Model, Register, Login  Encrypt via JSONWT

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+express = require('express');
+const { register, login } = require('../controllers/auth');
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+
+module.exports = router;
