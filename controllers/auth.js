25c3d9aa6df56a51e0c2a37527de778d1fd31514
j4v3d <javedrealtor@gmail.com>
2023-02-12 19:29:05 -0500

    Forgot Password Generate Taken

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,6 +47,39 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc    Current logged in user
+// @route   POST /api/v1/auth/me
+// @access  Public
+
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  //  .select('-password');
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+// @desc    Forgot password logged in user
+// @route   POST /api/v1/auth/forgotpassword
+// @access  Public
+
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email', 404));
+  }
+
+  // Get Reset Token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
@@ -68,15 +101,3 @@ const sendTokenResponse = (user, statusCode, res) => {
     token,
   });
 };
-// @desc    Current logged in user
-// @route   POST /api/v1/auth/me
-// @access  Public
-
-exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
-  //  .select('-password');
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
-});
