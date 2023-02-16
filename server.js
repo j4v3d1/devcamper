8a0c8175adb0cdb9cd0c56ed71ce0651059a5653
j4v3d <javedrealtor@gmail.com>
2023-02-16 12:17:28 -0500

    Logout, Clr Token, NoSQL, XSS Protect, hpp, CORS

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const xss = require('xss-clean');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
-const fileupload = require('express-fileupload');
+const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 
 //Load config file (env vars)
@@ -35,7 +41,29 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 //File Uploading middleware
-app.use(fileupload());
+app.use(fileUpload());
+
+//Sanitize data
+app.use(mongoSanitize());
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+
+// Prevent parameter pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
